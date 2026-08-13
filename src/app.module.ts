236b5c1aa5613631125f6a372b111fd60c6365ba
juan.quiroz <jq8069580@gmail.com>
2026-08-13 10:40:42 -0500

    chore(app): registrar los nuevos modulos en AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AddressesModule } from './addresses/addresses.module';
+import { AdminModule } from './admin/admin.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CatalogModule } from './catalog/catalog.module';
+import { CheckoutModule } from './checkout/checkout.module';
+import { OrdersModule } from './orders/orders.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductsModule } from './products/products.module';
+import { UploadModule } from './upload/upload.module';
+import { UsersModule } from './users/users.module';
+import { WebhookModule } from './webhook/webhook.module';
 
 @Module({
   imports: [
@@ -14,6 +21,13 @@ import { ProductsModule } from './products/products.module';
     AuthModule,
     ProductsModule,
     CatalogModule,
+    AddressesModule,
+    OrdersModule,
+    CheckoutModule,
+    WebhookModule,
+    UploadModule,
+    AdminModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
